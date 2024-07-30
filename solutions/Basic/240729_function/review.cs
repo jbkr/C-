69f21ecf7b1656c2ba6b07ab37c9ac31ac9b068a
jb <jb_kr@yahoo.co.kr>
2024-07-30 13:48:03 +0900

    review

---
+++ solutions/Basic/240729_function/review.cs
@@ -0,0 +1,32 @@
+﻿class Cat
+{
+    string name="";
+    string color="";
+
+    public Cat()
+    {
+        this.name = "Hello";
+    }
+
+    public Cat(string color): this()
+    {
+        
+        this.color = color;
+    }
+
+    ~Cat() { }
+
+    public void Meow()
+    {
+        Console.WriteLine("야옹");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cat cat = new Cat("Gold");
+        cat.Meow();
+    }
+}
