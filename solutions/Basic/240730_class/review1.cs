e4f7435b2a1f62bbe57b55d29f82d11e9b722792
jb <jb_kr@yahoo.co.kr>
2024-07-31 17:01:43 +0900

    review

---
+++ solutions/Basic/240730_class/review1.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+
+class PlayerManager
+{
+    static PlayerManager? instance;
+    private PlayerManager() { }
+    public static PlayerManager Instance()
+    {
+        if (instance == null)
+        {
+            instance = new PlayerManager();
+        }
+        return instance;
+    }
+    Player[] players = new Player[100];
+    public void Add(Player player)
+    {
+        int index = 0;
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (players[i] == null)
+            {
+                index = i;
+                break;
+            }
+        }
+        players[index] = player;
+    }
+    public void Remove(string name)
+    {
+        Player? player = Find(name);
+        //int index=players.
+    }
+    public Player? Find(string name)
+    {
+        foreach (Player item in players)
+        {
+            if (item.Name.Equals(name))
+            {
+                return item;
+            }
+        }
+        return null;
+    }
+    public void Show()
+    {
+        foreach (Player item in players)
+        {
+            item?.ShowInfo();
+        }
+    }
+    public void KK()
+    {
+
+    }
+}
+
+class Position
+{
+    int x;
+    int y;
+    public Position(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    public void Set(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Player
+{
+    string name;
+    int level;
+    Position position;
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    public Player(string name, int level, Position position)
+    {
+        this.name = name;
+        this.level = level;
+        this.position = position;
+    }
+    public void LevelUp()
+    {
+        this.level = 0;
+    }
+    public void Move()
+    {
+
+    }
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Player name: {name}, Player level: {level}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PlayerManager.Instance().Add(new Player("Superman", 1, new Position(0, 0)));
+        PlayerManager.Instance().Add(new Player("Ironman", 2, new Position(5, 10)));
+        PlayerManager.Instance().Add(new Player("Spiderman", 5, new Position(10, 10)));
+
+        PlayerManager.Instance().Show();
+
+        Player? player = PlayerManager.Instance().Find("Superman");
+        player?.ShowInfo();
+
+    }
+}
---
+++ solutions/Basic/240730_class/review2.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = new int[3];
+        foreach (var item in arr)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
