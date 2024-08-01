effae71b8d3d000b8d89c88c4cdaf02fc9adf2cb
jb <jb_kr@yahoo.co.kr>
2024-08-01 13:42:18 +0900

    review

---
+++ solutions/Basic/240731_class/review.cs
@@ -0,0 +1,53 @@
+﻿
+class Mammal
+{
+    public virtual void Eat()
+    {
+        Console.WriteLine("Mammal");
+    }
+}
+
+class Dog : Mammal
+{
+    public sealed override void Eat()
+    {
+        Console.WriteLine("Dog");
+    }
+}
+
+class Puppy : Dog
+{
+    public override void Eat()
+    {
+
+    }
+}
+
+class Cat : Mammal
+{
+    public override void Eat()
+    {
+        Console.WriteLine("Cat");
+    }
+}
+
+class Program
+{
+    static void Care(Mammal mammal)
+    {
+        mammal.Eat();
+    }
+
+    static void Main(string[] args)
+    {
+        Mammal[] mammals = { new Dog(), new Cat() };
+
+        foreach (var item in mammals)
+        {
+            item.Eat();
+        }
+
+        Care(new Cat());
+
+    }
+}
