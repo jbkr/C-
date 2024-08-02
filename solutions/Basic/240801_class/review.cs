cd337b08e00248ded4f6acfaa4b71c86db7c18bb
jb <jb_kr@yahoo.co.kr>
2024-08-02 11:10:30 +0900

    review

---
+++ solutions/Basic/240801_class/review.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    struct Member
+    {
+        public string Name;
+        public int Age;
+    }
+
+    static Member Func(Member member) { return member; }
+
+    static void Main(string[] args)
+    {
+        Member member;
+        member.Name = "test";
+        member.Age = 5;
+
+        Member member1 = Func(member);
+
+        var person = ("Warrior", 10);
+        Console.WriteLine(person.Item1);
+        Console.WriteLine(person.Item2);
+
+        var warrior = (name: "gundam", level: 34);
+        Console.WriteLine(warrior.name);
+        Console.WriteLine(warrior.level);
+
+        var (Name, Level) = warrior;
+        Console.WriteLine(Name);
+        Console.WriteLine(Level);
+    }
+}
