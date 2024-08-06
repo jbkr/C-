270fcea5c63964526c6b49dd48ae066b9aa0031d
jb <jb_kr@yahoo.co.kr>
2024-08-06 16:43:46 +0900

    review

---
+++ solutions/Intermediate/240805_interface/review.cs
@@ -0,0 +1,27 @@
+﻿class Alpha : ICloneable
+{
+    public string A;
+    public string B;
+
+    public object Clone()
+    {
+        return new Alpha() { A = this.A, B = this.B };
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Alpha obj1 = new Alpha() { A = "Hello", B = "World" };
+        Alpha obj2 = obj1;
+        Alpha? obj3 = obj1.Clone() as Alpha;
+        Console.WriteLine(obj1.A + " " + obj1.B);
+
+        obj2.B = "Man";
+        Console.WriteLine(obj1.A + " " + obj1.B);
+
+        Console.WriteLine(obj3.A + " " + obj3.B);
+
+    }
+}
