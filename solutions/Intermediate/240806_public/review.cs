a3655c4182eabdc754758b814c95d98ec68da2b4
jb <jb_kr@yahoo.co.kr>
2024-08-07 16:21:29 +0900

    review

---
+++ solutions/Intermediate/240806_public/review.cs
@@ -0,0 +1,14 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int[] arr2 = new int[] { 1, 2, 3, 4, 5 };
+        int[] arr3 = new int[5] { 1, 2, 3, 4, 5 };
+
+        Array.Sort(arr);
+        Array.Reverse(arr);
+        Array.ForEach(arr, a => Console.WriteLine(a));
+    }
+}
