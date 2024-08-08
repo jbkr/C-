b93c71ded0a9374935a337bd590c341cdb7d0976
jb <jb_kr@yahoo.co.kr>
2024-08-08 13:17:12 +0900

    review

---
+++ solutions/Intermediate/240807_collection/review.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s = string.Empty;
+        for (int i = 0; i < 10; i++)
+        {
+            s += i.ToString();
+        }
+        Console.WriteLine(s);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 10; i++)
+        {
+            stringBuilder.Append(i.ToString());
+        }
+        Console.WriteLine(stringBuilder[0]);
+    }
+}
