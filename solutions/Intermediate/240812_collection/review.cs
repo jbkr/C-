f1700cb3f8e18c102e64ff5e5a69d66592b90878
Lenovo <jb_kr@yahoo.co.kr>
2024-08-13 19:06:41 +0900

    review

--- solutions/Intermediate/240812_collection/review.cs
+++ solutions/Intermediate/240812_collection/review.cs
@@ -1,9 +1,18 @@
 ﻿class Program
 {
+    static int Plus(int a, int b) { return a + b; }
 
+    delegate int Del(int a, int b);
 
     static void Main(string[] args)
     {
-        
+        Del del = Plus;
+        Console.WriteLine(del(1, 2));
+        Func(del);
+    }
+
+    static void Func(Del del)
+    {
+        Console.WriteLine(del(2, 5));
     }
 }
