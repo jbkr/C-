636c282c45fe14f20fb6e98e4ad2b3f52b61a777
jb <jb_kr@yahoo.co.kr>
2024-08-15 15:27:22 +0900

    review

---
+++ solutions/Intermediate/240814_event/review.cs
@@ -0,0 +1,16 @@
+﻿
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        int[] arr = { 12, 2, 3, 5, 7 };
+
+        int[] arr2 = Array.FindAll(arr, a => a > 10);
+
+        foreach (int i in arr2)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
