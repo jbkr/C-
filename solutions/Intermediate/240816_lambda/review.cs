a128cce6effcfa39979c23c4c15099daa3e66539
jb <jb_kr@yahoo.co.kr>
2024-08-17 13:59:01 +0900

    review

---
+++ solutions/Intermediate/240816_lambda/review.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Predicate<int> predicate = n => n > 5;
+        Console.WriteLine(predicate(20));
+
+        Predicate<string> predicate1 = s => s.StartsWith("A");
+        Console.WriteLine(predicate1("Apple"));
+    }
+
+}
