fa0c4555e08c8a27aee278a93582c95154d26de2
Lenovo <jb_kr@yahoo.co.kr>
2024-08-25 14:11:11 +0900

    review

---
+++ solutions/Intermediate/240823_LINQ/review.cs
@@ -0,0 +1,22 @@
+﻿class MyClass
+{
+    [Obsolete("This method is going to be deprecated. Use NewMethod")]
+    public void OldMethod()
+    {
+        Console.WriteLine("I'm old");
+    }
+
+    public void NewMethod()
+    {
+        Console.WriteLine("I'm new");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass myClass = new MyClass();
+        myClass.OldMethod();
+    }
+}
