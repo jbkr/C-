c8a0445b13ae8ddb7b8ce3221b4ce5ef00bdf508
jb <jb_kr@yahoo.co.kr>
2024-08-27 10:51:17 +0900

    review

--- solutions/Intermediate/240826_etc/CodeFile8.cs
+++ solutions/Intermediate/240826_etc/CodeFile8.cs
@@ -8,3 +8,35 @@
 // PlayerPrefs
 // Save, Load
 // class -> json -> class
+
+using System.Text.Json;
+
+namespace SerializeBasic
+{
+    public class WeatherForecast
+    {
+        public DateTimeOffset Date { get; set; }
+        public int TemperatureCelcius { get; set; }
+        public string? Summary { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var weatherForecast = new WeatherForecast
+            {
+                Date = DateTime.Parse("2019-08-01"),
+                TemperatureCelcius = 25,
+                Summary = "Hot"
+            };
+
+            string fileName = "WeatherForecast.json";
+            string jsonString = JsonSerializer.Serialize(weatherForecast);
+            File.WriteAllText(fileName, jsonString);
+
+            Console.WriteLine(File.ReadAllText(fileName));
+        }
+    }
+}
+
--- solutions/Intermediate/240826_etc/review.cs
+++ solutions/Intermediate/240826_etc/review.cs
@@ -1,1 +1,31 @@
-﻿
+﻿class Program
+{
+    static void Func(object obj)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Hello Thread " + obj);
+            Thread.Sleep(10);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        // Thread
+        Thread thread = new Thread(() => Func("Good"));
+        thread.Start();
+
+        // Main
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Hello Main");
+            Thread.Sleep(10);
+        }
+
+        thread.Join();
+
+        Console.WriteLine("End");
+
+
+    }
+}
