a5d01c9679267195b98372ed0c15fe14568725f8
jb_kr <jb_kr@DESKTOP-K9VKLDU>
2025-01-09 16:50:59 +0900

    select

--- solutions/Intermediate/240822_1_LINQ/07_LINQ.cs
+++ solutions/Intermediate/240822_1_LINQ/07_LINQ.cs
@@ -70,81 +70,81 @@ namespace MyCSharp // LINQ 활용
     }
 }
 
-// LINQ 활용 
+//// LINQ 활용 
 
 
-//첫번째는 가독성. LINQ는 “깊은 코드”를 막아준다 ----------------------------------------------
+////첫번째는 가독성. LINQ는 “깊은 코드”를 막아준다 ----------------------------------------------
 
-    var sheet = GetSheet();
+//    var sheet = GetSheet();
 
-    foreach (var row in sheet)
-    {
-        if ((row[0] as string) == name)
-        {
-            var points = row.Skip(1);
-            var pointList = new List<string>();
-            foreach (var point in points)
-            {
-                pointList.Add(int.Parse((point as string) ?? "0"));
-            }
-            return (pointList[0], pointList[1]);
-        }
-    }
+//    foreach (var row in sheet)
+//    {
+//        if ((row[0] as string) == name)
+//        {
+//            var points = row.Skip(1);
+//            var pointList = new List<string>();
+//            foreach (var point in points)
+//            {
+//                pointList.Add(int.Parse((point as string) ?? "0"));
+//            }
+//            return (pointList[0], pointList[1]);
+//        }
+//    }
 
-    var result = sheet
-        .Where(x => (x[0] as string) == name)
-        .FirstOrDefault()
-        .Skip(1)
-        .Select(x => int.Parse((x as string) ?? "0"))
-        .ToArray();
-    return (result[0], result[1]);
-
-
-//두번째 이유는 확장 메서드 -------------------------------------------------------------------
-
-    public static class LINQExtension
-    {
-        public static void ForEach<T>(this IEnumerable<T> list, Action<T> action)
-        {
-            foreach (var item in list)
-                action(item);
-        }
-    }
-
-    // SQL문법과 비슷한 C# 문법을 제공한다는 점에서 많은 편리함을 제공하지만, 
-    // 실제 .NET CLR 에서는 이러한 쿼리들이 메서드 형태로 호출되어야 하므로, 
-    // 이러한 쿼리들은 C# 컴파일시에
-    // 내부적으로 확장메서드(Extension Method)로 변경되어진다.
-
-    // 쿼리식 표현
-    var v = from ord in db.Orders
-            where ord.Customer_ID == "FRANS"
-            orderby ord.Order_Date ascending
-            select ord;
-
-    // 메서드식 표현1
-    var v = db.Orders.Where(order => order.Customer_ID == "FRANS").Select(o => o);
-
-    // 메서드식 표현2
-    // 위의 메서드식 표현1의 Select()메서드는 동일데이타를 변형없이 리턴하므로 생략가능
-    var v = db.Orders.Where(order => order.Customer_ID == "FRANS");
+//    var result = sheet
+//        .Where(x => (x[0] as string) == name)
+//        .FirstOrDefault()
+//        .Skip(1)
+//        .Select(x => int.Parse((x as string) ?? "0"))
+//        .ToArray();
+//    return (result[0], result[1]);
 
 
-//Lazy Evaluation ---------------------------------------------------------------------------
+////두번째 이유는 확장 메서드 -------------------------------------------------------------------
 
-    //  Lazy Evaluation이란 컬렉션의 정의만을 설정해두고 내부의 값을 정의는 해당 값이 호출될 때까지 미루는 것
-    //  이러한 Lazy Evaluation을 통해서 불필요한 컬렉션의 호출과 연산을 생략할 수 있다.
+//    public static class LINQExtension
+//    {
+//        public static void ForEach<T>(this IEnumerable<T> list, Action<T> action)
+//        {
+//            foreach (var item in list)
+//                action(item);
+//        }
+//    }
 
-    var list = new int[] { 10, 20, 30, 40, 45, 50 };
-    var result = list
-        .Where(x => {
-            Console.WriteLine($"[Where] {x}");
-            return x > 30;
-        })
-        .Select(x => {
-            Console.WriteLine($"[Select] {x}");
-            return x * x;
-        });
+//    // SQL문법과 비슷한 C# 문법을 제공한다는 점에서 많은 편리함을 제공하지만, 
+//    // 실제 .NET CLR 에서는 이러한 쿼리들이 메서드 형태로 호출되어야 하므로, 
+//    // 이러한 쿼리들은 C# 컴파일시에
+//    // 내부적으로 확장메서드(Extension Method)로 변경되어진다.
 
-    Console.WriteLine(string.Join(",", result));  //출력전에는 연산 안함.
+//    // 쿼리식 표현
+//    var v = from ord in db.Orders
+//            where ord.Customer_ID == "FRANS"
+//            orderby ord.Order_Date ascending
+//            select ord;
+
+//    // 메서드식 표현1
+//    var v = db.Orders.Where(order => order.Customer_ID == "FRANS").Select(o => o);
+
+//    // 메서드식 표현2
+//    // 위의 메서드식 표현1의 Select()메서드는 동일데이타를 변형없이 리턴하므로 생략가능
+//    var v = db.Orders.Where(order => order.Customer_ID == "FRANS");
+
+
+////Lazy Evaluation ---------------------------------------------------------------------------
+
+//    //  Lazy Evaluation이란 컬렉션의 정의만을 설정해두고 내부의 값을 정의는 해당 값이 호출될 때까지 미루는 것
+//    //  이러한 Lazy Evaluation을 통해서 불필요한 컬렉션의 호출과 연산을 생략할 수 있다.
+
+//    var list = new int[] { 10, 20, 30, 40, 45, 50 };
+//    var result = list
+//        .Where(x => {
+//            Console.WriteLine($"[Where] {x}");
+//            return x > 30;
+//        })
+//        .Select(x => {
+//            Console.WriteLine($"[Select] {x}");
+//            return x * x;
+//        });
+
+//    Console.WriteLine(string.Join(",", result));  //출력전에는 연산 안함.
 
